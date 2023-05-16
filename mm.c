f1e1ac428be0f02297df2e3ba3031fe6528a387f
WaokE <dlgudwns1207@gmail.com>
2023-05-16 02:27:57 +0000

    feat: changed constants, macros and find_fit function for explicit

--- mm.c
+++ mm.c
@@ -75,7 +75,8 @@ void *mm_realloc(void *ptr, size_t size);
  * mm_init - initialize the malloc package.
  */
 static char *heap_listp;
-static void *recently_allocated = NULL; 
+static char *head;
+static char *tail;
 int mm_init(void)
 {   
     // 힙 생성과 유효성 검사
@@ -161,7 +162,6 @@ static void *coalesce(void *bp)
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0)); // 다음 블록의 푸터에 할당되지 않음 상태와 사이즈를 담아줌
         bp = PREV_BLKP(bp); // 블록의 포인터를 이전 블록으로 옮김
     }
-    recently_allocated = bp;
     return bp; // 블록의 포인터를 리턴
 }
 
@@ -179,8 +179,8 @@ void *mm_malloc(size_t size)
     if (size == 0){return NULL;}
     
     // 입력받은 사이즈에 따라 헤더와 푸터를 위한 공간을 할당하고, 더블 워드 조건을 만족시킨다.
-    if (size <= DSIZE){asize = 2*DSIZE;}
-    else{asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);}
+    if (size <= 2*DSIZE){asize = 3*DSIZE;}
+    else{asize = DSIZE * ((size + (2*DSIZE) + (DSIZE-1)) / DSIZE);}
 
     // 할당할 곳을 탐색하고, 성공적으로 찾아냈다면 메모리를 할당해준다.
     if ((bp = find_fit(asize)) != NULL){
@@ -188,7 +188,7 @@ void *mm_malloc(size_t size)
         return bp;
     }
     
-    // 
+    // 할당할 곳이 없다면 힙 사이즈를 늘린다.
     extendsize = MAX(asize, CHUNKSIZE);
     if ((bp = extend_heap(extendsize/WSIZE)) == NULL){return NULL;}
     place(bp, asize);
@@ -196,26 +196,13 @@ void *mm_malloc(size_t size)
 }
 
 static void *find_fit(size_t asize){
-    void *bp;
-
-    if (recently_allocated == NULL) {
-        recently_allocated = heap_listp;
-    }
-
-    for (bp = recently_allocated; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
-         if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
-            recently_allocated = bp;
+    void *bp = head;
+    do{
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
             return bp;
-            }
-    }
-
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
-         if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
-            recently_allocated = bp;
-            return bp;
-            }
-    }
-
+        }
+        bp = (NEXT_FB(bp));
+    } while (bp != tail);
     return NULL;
 }
 
